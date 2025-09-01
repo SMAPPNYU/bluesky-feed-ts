a3fa72140c42e26d2e77c368770e0b6a5ee06277
mjheseltine <65302937+mjheseltine@users.noreply.github.com>
2025-09-01 10:41:01 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,9 @@ export class FeedGenerator {
   static create(cfg: Config) {
     const app = express()
     const db = createDb(cfg.sqliteLocation)
-    const firehose = new FirehoseSubscription(db, cfg.subscriptionEndpoint)
+
+    // ⛔ disable firehose for now
+    // const firehose = new FirehoseSubscription(db, cfg.subscriptionEndpoint)
 
     const didCache = new MemoryCache()
     const didResolver = new DidResolver({
@@ -40,34 +42,24 @@ export class FeedGenerator {
       didCache,
     })
 
-    const server = createServer({
-      validateResponse: true,
-      payload: {
-        jsonLimit: 100 * 1024, // 100kb
-        textLimit: 100 * 1024, // 100kb
-        blobLimit: 5 * 1024 * 1024, // 5mb
-      },
-    })
-    const ctx: AppContext = {
-      db,
-      didResolver,
-      cfg,
-    }
+    const server = createServer({ ... })
+    const ctx: AppContext = { db, didResolver, cfg }
     feedGeneration(server, ctx)
     describeGenerator(server, ctx)
     app.use(server.xrpc.router)
     app.use(wellKnown(ctx))
 
-    return new FeedGenerator(app, db, firehose, cfg)
+    // return FeedGenerator(app, db, firehose, cfg)
+    return new FeedGenerator(app, db, null as any, cfg) // 👈 hack in null
   }
 
   async start(): Promise<http.Server> {
     await migrateToLatest(this.db)
-    this.firehose.run(this.cfg.subscriptionReconnectDelay)
+    // ⛔ disable firehose runner
+    // this.firehose.run(this.cfg.subscriptionReconnectDelay)
     this.server = this.app.listen(this.cfg.port, this.cfg.listenhost)
     await events.once(this.server, 'listening')
     return this.server
   }
-}
 
 export default FeedGenerator
