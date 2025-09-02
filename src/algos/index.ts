83842f0263389eefbf80806e72934d806d04ea46
mjheseltine <65302937+mjheseltine@users.noreply.github.com>
2025-09-02 15:25:45 +0100

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,15 +4,14 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 
-// Existing feed
 import * as csmapFeed from './csmap-feed'
-import * as debateFeed from './debate-feed'   // <-- this is enough
+import * as debateFeed from './debate-feed'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [csmapFeed.shortname]: csmapFeed.handler,
-  [debateFeed.shortname]: debateFeed.handler,   // <-- consistent with csmap
+  [debateFeed.shortname]: debateFeed.handler,
 }
 
 export default algos
